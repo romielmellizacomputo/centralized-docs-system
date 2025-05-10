6624312bfd50de43d5fab3cd9d91caed6209001b
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-11 06:34:10 +0800

    Delete SheetSync/fetchallissues.js

--- SheetSync/fetchallissues.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-// Fetch the GitLab issues data from your Google Apps Script endpoint
-const url = process.env.FETCH_ALL_GITLAB_ISSUES_SERVICE;
-
-fetch(url)
-  .then(res => res.text()) // Expecting text response from the Apps Script
-  .then(result => console.log(result))  // Log the result (e.g., ✅ Sync successful)
-  .catch(err => console.error('❌ Error:', err)); // Handle any errors
