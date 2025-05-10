1d11b0973d3a802b5c689bc44c70f14b072da11a
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-11 05:26:59 +0800

    Update fetch-ntc.js

--- team-cds/fetch-ntc.js
+++ team-cds/fetch-ntc.js
@@ -1,1 +1,4 @@
-
+fetch('https://script.google.com/macros/s/AKfycbxby6scKsSziC5LBr2kFgd5_0UCx56X6K8qyuDbZNYxJBu_nhln2oDYQBiFpDAOHrmjyQ/exec')
+  .then(res => res.text())
+  .then(result => console.log(result))  // You'll see either '✅ Sync successful' or error message
+  .catch(err => console.error('❌ Error:', err));
