df910522c25acfc37746b0335cf7dabac9689b6c
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-11 04:56:37 +0800

    Update fetch-issues.js

--- team-cds/fetch-issues.js
+++ team-cds/fetch-issues.js
@@ -1,4 +1,4 @@
-fetch('https://script.google.com/macros/s/AKfycbwXt9aUsRGDMxxxmFPW_2UngHFJKbutzBWZaNiXn4TQD-aUmKATpGe2DcPuh3A7xKV9/exec')
+fetch('https://script.google.com/macros/s/AKfycbzAmAMI-NGzhTQd4polExAvF-FkcjucVOP2tWF4Vs-RbvxYbcF1NOCQAwFqyHP0tHZp/exec')
   .then(res => res.text())
   .then(result => console.log(result))  // You'll see either '✅ Sync successful' or error message
   .catch(err => console.error('❌ Error:', err));
