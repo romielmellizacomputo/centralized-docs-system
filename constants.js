615dc29ddb6d580246bee14c38656e6cb62bd00a
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-18 06:32:32 +0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -7,4 +7,6 @@ export const NTC_SHEET = 'NTC';
 export const DASHBOARD_SHEET = 'Dashboard';
 
 export const CENTRAL_ISSUE_SHEET_ID = process.env.SHEET_SYNC_SID;
-export const ALL_ISSUES_RANGE = 'ALL ISSUES!C4:U';
+export const ALL_ISSUES = 'ALL ISSUES!C4:U';
+export const ALL_MR = 'ALL MRs!C4:O';
+
