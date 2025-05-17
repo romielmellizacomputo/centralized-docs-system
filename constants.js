1866a87de82e509d99eef946fd6739adacaf5c44
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-18 06:40:04 +0800

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,12 +1,14 @@
-
+// Source 001
 export const UTILS_SHEET_ID = process.env.LEADS_CDS_SID;
+
+// Source 002
+export const CENTRAL_ISSUE_SHEET_ID = process.env.SHEET_SYNC_SID;
+export const ALL_ISSUES = 'ALL ISSUES!C4:U';
+export const ALL_MR = 'ALL MRs!C4:O';
+
+// Target
 export const G_MILESTONES = 'G-Milestones';
 export const G_ISSUES_SHEET = 'G-Issues';
 export const G_MR_SHEET = 'G-MR';
 export const NTC_SHEET = 'NTC';
 export const DASHBOARD_SHEET = 'Dashboard';
-
-export const CENTRAL_ISSUE_SHEET_ID = process.env.SHEET_SYNC_SID;
-export const ALL_ISSUES = 'ALL ISSUES!C4:U';
-export const ALL_MR = 'ALL MRs!C4:O';
-
