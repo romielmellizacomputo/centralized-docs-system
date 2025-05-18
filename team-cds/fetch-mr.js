e5ccd1d726cd97635d0ab93d144545e7a6b8e281
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-18 08:13:14 +0800

    Update fetch-mr.js

--- team-cds/fetch-mr.js
+++ team-cds/fetch-mr.js
@@ -9,37 +9,12 @@ import {
   generateTimestampString
 } from '../constants.js';
 
-async function authenticate() {
-  const credentials = JSON.parse(process.env.TEAM_CDS_SERVICE_ACCOUNT_JSON);
-  const auth = new google.auth.GoogleAuth({
-    credentials,
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
-  return auth;
-}
-
-async function getSheetTitles(sheets, spreadsheetId) {
-  const res = await sheets.spreadsheets.get({ spreadsheetId });
-  const titles = res.data.sheets.map(sheet => sheet.properties.title);
-  console.log(`📄 Sheets in ${spreadsheetId}:`, titles);
-  return titles;
-}
-
-async function getAllTeamCDSSheetIds(sheets) {
-  const { data } = await sheets.spreadsheets.values.get({
-    spreadsheetId: UTILS_SHEET_ID,
-    range: 'UTILS!B2:B',
-  });
-  return data.values?.flat().filter(Boolean) || [];
-}
-
-async function getSelectedMilestones(sheets, sheetId) {
-  const { data } = await sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: `${G_MILESTONES}!G4:G`,
-  });
-  return data.values?.flat().filter(Boolean) || [];
-}
+import {
+  authenticate,
+  getSheetTitles,
+  getAllTeamCDSSheetIds,
+  getSelectedMilestones,
+} from './common.js';
 
 async function getAllIssues(sheets) {
   const { data } = await sheets.spreadsheets.values.get({
