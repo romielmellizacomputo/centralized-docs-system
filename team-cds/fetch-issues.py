e4673d4e8d2b6a7212400bd8fc69751c87bec945
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-22 01:26:33 +0800

    Update fetch-issues.py

--- team-cds/fetch-issues.py
+++ team-cds/fetch-issues.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the root directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from googleapiclient.discovery import build
 from constants import (
     UTILS_SHEET_ID,
