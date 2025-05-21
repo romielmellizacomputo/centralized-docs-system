2f3483652067803ead08727139cd7e622e45e065
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-22 01:45:18 +0800

    Update fetch-issues.py

--- team-cds/fetch-issues.py
+++ team-cds/fetch-issues.py
@@ -1,8 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import os
 from googleapiclient.discovery import build
 from common import (
     authenticate,
