2c6dd9d1bf39661513733f0a5ff8d18b338c7d85
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-22 01:44:58 +0800

    Update fetch-mr.py

--- team-cds/fetch-mr.py
+++ team-cds/fetch-mr.py
@@ -1,7 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
+import os
 from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
 from constants import (
     UTILS_SHEET_ID,
     G_MILESTONES,
